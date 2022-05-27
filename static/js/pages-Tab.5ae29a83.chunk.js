290153aca55659521576b6ec13964beb112dff44
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2022-05-27 13:10:01 +0530

    Updates

---
+++ static/js/pages-Tab.5ae29a83.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_components=self.webpackChunkreact_components||[]).push([[3193],{4079:function(n,e,t){t.r(e);var c=t(70885),r=t(72791),a=t(59930),u=t(80184),s=["Tab One","Tab Two","Tab Three"];e.default=function(){var n=(0,r.useState)(0),e=(0,c.Z)(n,2),t=e[0],i=e[1],o=function(n){return function(){i(n)}};return(0,u.jsx)("div",{children:(0,u.jsx)(a.Tab,{tab:t,children:s.map((function(n,e){return(0,u.jsx)(a.Tab.Item,{onClick:o(e),children:(0,u.jsx)("span",{children:n})},e)}))})})}}}]);
+//# sourceMappingURL=pages-Tab.5ae29a83.chunk.js.map
